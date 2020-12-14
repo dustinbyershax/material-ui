1eb26781384f3bb1a0ba38d5f77aada0b6e2527d
Dustin Byers <dbyers84@gmail.com>
2020-12-14 05:49:02 -0800

    Update TransitionHover.js

--- docs/src/pages/customization/transitions/TransitionHover.js
+++ docs/src/pages/customization/transitions/TransitionHover.js
@@ -16,7 +16,7 @@ const customTheme = createMuiTheme({
   },
 });
 
-const Styledvatar = styled(Avatar)`
+const StyledAvatar = styled(Avatar)`
   ${({ theme }) => `
   cursor: pointer;
   background-color: ${theme.palette.primary.main};
@@ -35,7 +35,7 @@ export default function TransitionHover() {
     <NoSsr>
       <MuiThemeProvider theme={customTheme}>
         <ThemeProvider theme={customTheme}>
-          <Styledvatar>OP</Styledvatar>
+          <StyledAvatar>OP</Styledvatar>
         </ThemeProvider>
       </MuiThemeProvider>
     </NoSsr>
